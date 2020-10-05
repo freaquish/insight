3c4ce9a33e3b7bb72c1061a61c92bea6a542ae52
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2020-10-06 00:26:01 +0530

    remove console logs

--- insight/.history/components/BottomBar_20200923025327.vue
+++ insight/.history/components/BottomBar_20200923025327.vue
@@ -1,34 +1,35 @@
 <template>
- <client-only>
-  <div class="fixed bg-white flex flex-row justify-around w-full h-10 py-2 bg-white">
+  <client-only>
+    <div
+      class="fixed bg-white flex flex-row justify-around w-full h-10 py-2 bg-white"
+    >
       <div>
-          <span class="material-icons">home</span>
+        <span class="material-icons">home</span>
       </div>
-  </div>
- </client-only>
+    </div>
+  </client-only>
 </template>
 
 <script>
-import HomeSvg from "@/assets/svg/HomeSvg.vue";
-import ExploreSvg from "@/assets/svg/ExploreSvg";
-import NotificationSvg from "@/assets/svg/NotificationSvg.vue";
-import SettingsSvg from "@/assets/svg/SettingsSvg.vue";
+import HomeSvg from '@/assets/svg/HomeSvg.vue'
+import ExploreSvg from '@/assets/svg/ExploreSvg'
+import NotificationSvg from '@/assets/svg/NotificationSvg.vue'
+import SettingsSvg from '@/assets/svg/SettingsSvg.vue'
 export default {
-    components:{
-        HomeSvg,
-        ExploreSvg,
-        NotificationSvg,
-        SettingsSvg,
-    },
-    data(){
-        return{
-            routeName: undefined
-        }
-    },
-    updated(){
-        this.routeName = this.$route.name;
-        console.log(this.routeName);
-        
+  components: {
+    HomeSvg,
+    ExploreSvg,
+    NotificationSvg,
+    SettingsSvg
+  },
+  data() {
+    return {
+      routeName: undefined
     }
+  },
+  updated() {
+    this.routeName = this.$route.name
+    // console.log(this.routeName);
+  }
 }
 </script>
---
+++ insight/.history/components/editor_elements/SampleCard_20200904104152.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-20 h-16 mx-2 relative" @click="$emit('pick',index)">
+      <div class="absolute w-full h-full z-0">
+          <img v-if="this.asset.isTypeOf('image')" class="w-20 h-16" :src="asset.getSrc()" />
+          <video v-else-if="this.asset.isTypeOf('video')" class="w-20 h-16" :src="asset.getSrc()" />
+          <audio v-else-if="this.asset.isTypeOf('audio')" class="w-20 h-16" :src="asset.getSrc()" />
+      </div>
+      <div class="absolute w-full h-full z-10 bg-transparent"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['asset','index'],
+    mounted(){
+        console.log(this.asset, this.index);
+    }
+
+}
+</script>
+
+<style>
+
+</style>
