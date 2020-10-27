c65928ace8ce2c9b83dadb428c6614085327ae13
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2020-10-27 22:43:23 +0530

    See you

---
+++ insight/.history/pages/post/uploading_data_beta_20201024021638.vue
@@ -0,0 +1,14 @@
+<template> </template>
+
+<script lang="ts">
+import { mapState, mapActions } from 'vuex'
+import Vue from 'vue'
+
+export default Vue.extend({
+  computed: {
+    ...mapState('post/create_post', ['porgression'])
+  }
+})
+</script>
+
+<style scoped></style>
--- insight/.history/pages/post/uploading_data_beta_20201024021957.vue
+++ insight/.history/pages/post/uploading_data_beta_20201024021957.vue
@@ -1,12 +1,4 @@
-<template>
-  <div class="w-full h-full flex-col">
-    <div class="w-full h-10 flex px-2">
-      <span class="material-icons" @click="$router.go(-1)"
-        >keyboard_backspace</span
-      >
-    </div>
-  </div>
-</template>
+<template> </template>
 
 <script lang="ts">
 import { mapState, mapActions } from 'vuex'
