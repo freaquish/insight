3d0c95f9a348d7af492262c18a6e3ab6d4804e20
suyashmadhesia <suyashmadhesia@gmail.com>
2020-10-30 16:32:01 +0530

    newLoginPage

--- insight/pages/auth/logiin.vue
+++ insight/pages/auth/logiin.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="w-full h-screen overflow-hidden bck">
-    <div class="formpnl login100-form" v-show="showLoginForm">
+    <div class="formpnl" v-show="showLoginForm">
       <div v-if="this.error" class="h-12 rounded-lg w-full px-4 py-2 bg-red-600">
          <p class="font-lato font-bold text-white">Invalid Credentials, try again</p>
       </div>
-      <div class="title font-bold font-muli mt-4 text-green-400">freaquish</div>
-      <div class="w-auto h-12 mt-8 border rounded-md border-green-400">
-        <i class="fa fa-user-circle mt-4 text-white ml-2" aria-hidden="true"></i>
+      <div class="mx-auto w-32 h-32">
+        <img src="@/static/freaquishlogo.webp">
+      </div> 
+      <div class="title font-bold font-muli text-3xl mt-2">Freaquish</div>
+      <div class="w-auto mx-8 h-12 mt-8 border-b border-gray-600">
+        <i class="fa fa-user-circle mt-4 text-black ml-2" aria-hidden="true"></i>
         <input
-        class="outline-none bg-transparent ml-4 text-white font-muli"
+        class="outline-none bg-transparent ml-4 text-black font-muli"
         type="text" name="account_id"  placeholder="Phone Number" v-model="account_id">
       </div>
-      <div class="w-auto h-12 mt-4 border rounded-md border-green-400">
-        <i class="fa fa-key mt-4 text-white ml-2" aria-hidden="true"></i>
+      <div class="w-auto mx-8 h-12 mt-4 border-b border-gray-600">
+        <i class="fa fa-key mt-4 text-black ml-2" aria-hidden="true"></i>
         <input
-        class="outline-none bg-transparent ml-4 text-white font-muli"
+        class="outline-none bg-transparent ml-4 text-black font-muli"
         type="password" name="Password"  placeholder="Password" v-model="password">
       </div>
       <!-- <div class="text-right mt-4 mr-6 text-gray-600">Forget Password ?</div> -->
-      <div class="w-full h-16 py-2 px-2 mt-10 flex ">
-         <p @click="$router.push('/auth/register')" class="font-lato font-bold text-xl text-white mr-6 mt-3">Register</p>
-         <button @click="login" class="w-full py-2 font-muli font-bold text-white bg-green-400 rounded-lg">Login</button>
+      <div class="w-full h-16 py-2 px-2 mt-10 flex">
+
+         <div @click="login" class="lgnbtn w-full py-2 mx-5 font-muli font-bold focus:outline-none text-center text-white rounded-full"><h1 class="pt-1">LOGIN</h1></div>
+      </div>
+      <div class="ml-8 mr-8 text-center text-gray-600 mt-12 mb-8">
+        <div class="text-md">Don't have any account, join us <br></div>
+        <h1><a @click="$router.push('/auth/register')" class="mt-6 text-purple-500 font-bold text-xl font-muli">REGISTER</a></h1>
       </div>
     </div>
   </div>
@@ -82,31 +89,30 @@ export default {
 
 <style scoped>
 .formpnl {
-  width: 90%;
   margin: auto;
   margin-top: 15%;
-  background: transparent;
-  border-radius: 0.625rem;
   overflow: hidden;
 }
 .title {
   display: block;
-  /* font-family: 'Nunito',sans-serif;
-
+ font-family: 'Nunito',sans-serif;
   background-color: #f3ec78;
-  /* background-image: linear-gradient(45deg, #29013b,#378f0f,#ff38c3);
-  background-image: conic-gradient(45deg,  #29013b,#288000,#ff57cd); */
-  /* background-size: 100%;
+  background-image: linear-gradient(45deg, #7a13aa,#892cd4,#ff38c3);
+  background-image: conic-gradient(45deg,  #7a13aa,#892cd4,#ff57cd);
+  background-size: 100%;
   background-clip: text;
   -webkit-background-clip: text;
   -moz-background-clip: text;
   -webkit-text-fill-color: transparent;
-  -moz-text-fill-color: transparent; */
-  line-height: 1.2;
+  -moz-text-fill-color: transparent;
   text-align: center;
-    font-size: 39px;
+}
+.lgnbtn {
+  background-color: #f3ec78;
+  background-image: linear-gradient(45deg, #7a13aa,#892cd4,#ff38c3);
 }
 .bck{
-  background: #010141;
+  background: linear-gradient(16.1deg, rgba(0, 0, 0, 0) 0%, rgba(180, 179, 180, 0.51) 100%);
+  /* background: rgb(32, 32, 32); */
 }
 </style>
