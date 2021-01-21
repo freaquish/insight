c50b80bf971ed14a4f180d3364bc1f275278dcaa
Piyush jaiswal <iampiyushjaiswal103@gmail.com>
2021-01-22 03:18:49 +0530

    API url fix

--- insight/nuxt.config.js
+++ insight/nuxt.config.js
@@ -92,8 +92,8 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    // baseURL: 'https://condom.freaquish.com/api/v1/'
-    baseURL: 'http://localhost:8080/api/v1/'
+    baseURL: 'https://condom.freaquish.com/api/v1/'
+    // baseURL: 'http://localhost:8080/api/v1/'
   },
   router: {},
   /*
